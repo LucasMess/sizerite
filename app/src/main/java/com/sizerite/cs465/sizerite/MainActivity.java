205d602faa46fff98060dee868802cfa70ccae8f
Lucas Message <lucas.message@yahoo.com>
2017-11-07 19:27:27 -0800

    Initial commit.

---
+++ app/src/main/java/com/sizerite/cs465/sizerite/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sizerite.cs465.sizerite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
