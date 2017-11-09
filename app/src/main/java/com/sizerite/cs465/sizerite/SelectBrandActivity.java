24d5476b7af1e053c5835ac9c763760b91efd458
Lucas Message <lucas.message@yahoo.com>
2017-11-09 13:36:32 -0600

    Can add images and set the background color of the brand tiles.

--- app/src/main/java/com/sizerite/cs465/sizerite/SelectBrandActivity.java
+++ app/src/main/java/com/sizerite/cs465/sizerite/SelectBrandActivity.java
@@ -1,15 +1,18 @@
 package com.sizerite.cs465.sizerite;
 
 import android.content.Context;
-import android.media.Image;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -25,39 +28,66 @@ public class SelectBrandActivity extends AppCompatActivity {
 
         // Create a list of all the brands.
         List brands = new ArrayList<Brand>();
-        brands.add(new Brand("Nike", ""));
-        brands.add(new Brand("Adidas", ""));
-        brands.add(new Brand("Reebok", ""));
+        brands.add(new Brand("Nike", "#ffffff"));
+        brands.add(new Brand("Adidas", "#ffffff"));
+        brands.add(new Brand("Reebok", "#ffffff"));
 
         // Links the grid to the brand list.
-        BrandAdapter adapter = new BrandAdapter(this, (ArrayList<Brand>)brands);
+        BrandAdapter adapter = new BrandAdapter(this, (ArrayList<Brand>) brands);
         GridView grid = findViewById(R.id.brand_grid);
         grid.setAdapter(adapter);
+
+        // Set the on click listener for the items.
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+
+                // Clicked item.
+                Brand brand = (Brand) parent.getItemAtPosition(position);
+
+                // Start select category activity and pass the name of the brand selected.
+                Intent intent = new Intent(view.getContext(), SelectCategoryActivity.class);
+                intent.putExtra("brand_selected", brand.Name);
+                startActivity(intent);
+            }
+        });
 
     }
 
 
     // Adapter that converts a brand item to be shown in the grid view.
-    public class BrandAdapter extends ArrayAdapter<Brand>{
+    public class BrandAdapter extends ArrayAdapter<Brand> {
 
-        public BrandAdapter(Context context, ArrayList<Brand> brands){
+        public BrandAdapter(Context context, ArrayList<Brand> brands) {
             super(context, 0, brands);
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent){
+        public View getView(int position, View convertView, ViewGroup parent) {
+
             // Get the data item for this position
             Brand brand = getItem(position);
+
             // Check if an existing view is being reused, otherwise inflate the view
             if (convertView == null) {
                 convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_brand, parent, false);
             }
+
             // Lookup view for data population
             TextView brandName = (TextView) convertView.findViewById(R.id.brand_name);
             ImageView brandImage = (ImageView) convertView.findViewById(R.id.brand_image);
-            // Populate the data into the template view using the data object
+            RelativeLayout brandTile = (RelativeLayout) convertView.findViewById(R.id.brand_background);
+
+            // Set the tile text and color based on the brand.
             brandName.setText(brand.Name);
-            //brandName.setBackgroundResource();
+            brandTile.setBackgroundColor(Color.parseColor(brand.BackgroundColor));
+
+            int resId = getResources().getIdentifier(brand.Name.toLowerCase(), "drawable", getPackageName());
+
+            // If the resource ID is nul0, the file was not found, and the app will use the default image.
+            if (resId != 0) {
+                brandImage.setImageResource(resId);
+            }
+
 
             // Return the completed view to render on screen
             return convertView;
@@ -66,14 +96,19 @@ public class SelectBrandActivity extends AppCompatActivity {
 
     }
 
-    public class Brand{
+    public class Brand {
 
         public String Name;
-        public String ImageLocation;
+        public String BackgroundColor;
 
-        public Brand(String name, String imageLocation){
+        public Brand(String name) {
             Name = name;
-            ImageLocation = imageLocation;
+            BackgroundColor = "#ff7d32";
+        }
+
+        public Brand(String name, String backgroundColor){
+            this(name);
+            BackgroundColor = backgroundColor;
         }
     }
 }
