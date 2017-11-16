3cb4a17a616d4a6f3d87924a3eb4346fa87c8998
alan-fang <fang.alan.7@gmail.com>
2017-11-16 16:18:56 -0600

    Changed name of xml and .java files to match entire project, also added filler images to the layout

--- app/src/main/java/com/sizerite/cs465/sizerite/SelectFitActivity.java
+++ app/src/main/java/com/sizerite/cs465/sizerite/SelectFitActivity.java
@@ -1,8 +1,8 @@
 package com.sizerite.cs465.sizerite;
 
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -10,11 +10,11 @@ import android.widget.TextView;
  * Created by alanfang on 11/15/17.
  */
 
-public class FitSelectActivity extends AppCompatActivity {
+public class SelectFitActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fit_select);
+        setContentView(R.layout.activity_select_fit);
         setTitle("How well did it fit?");
 
         SeekBar fitSeekBar = (SeekBar) findViewById(R.id.seekbar);
@@ -26,29 +26,29 @@ public class FitSelectActivity extends AppCompatActivity {
                 @Override
                 public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                     TextView fitTextView = (TextView) findViewById(R.id.fitText);
+                    ImageView fitImageView = (ImageView) findViewById(R.id.imageFit);
                     if (i <= 3) {
                         fitTextView.setText("Tight fit");
+                        fitImageView.setImageResource(R.drawable.fillertight);
                     }
                     else if (i >= 4 && i <= 7){
                         fitTextView.setText("Normal fit");
+                        fitImageView.setImageResource(R.drawable.fillernormal);
                     }
                     else {
                         fitTextView.setText("Loose fit");
+                        fitImageView.setImageResource(R.drawable.fillerloose);
                     }
                 }
 
                 @Override
                 public void onStartTrackingTouch(SeekBar seekBar) {
-                    Rect currBounds = seekBar.getThumb().getBounds();
-                    currBounds.inset(30, 30);
-                    seekBar.getThumb().setBounds(currBounds);
+
                 }
 
                 @Override
                 public void onStopTrackingTouch(SeekBar seekBar) {
-                    Rect currBounds = seekBar.getThumb().getBounds();
-                    currBounds.inset(-30, -30);
-                    seekBar.getThumb().setBounds(currBounds);
+
                 }
             };
 }
