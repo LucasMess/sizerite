07ba88f75f560b17e2cd998190d0065eb7c56bc6
Lucas Message <lucas.message@yahoo.com>
2017-11-15 18:30:25 -0800

    Changed style of spinner.

--- app/src/main/java/com/sizerite/cs465/sizerite/SelectSizeActivity.java
+++ app/src/main/java/com/sizerite/cs465/sizerite/SelectSizeActivity.java
@@ -1,26 +1,37 @@
 package com.sizerite.cs465.sizerite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SelectSizeActivity extends AppCompatActivity {
 
+    final String DEFAULT_SELECTION = "Click here to select a size";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_size);
         setTitle("Select size");
 
-        Spinner spinner = (Spinner)findViewById(R.id.size_spinner);
+        final Spinner spinner = (Spinner)findViewById(R.id.size_spinner);
         ArrayAdapter<String> adapter;
         List<String> list;
 
+        // Add the entries to the list.
         list = new ArrayList<String>();
+        list.add(DEFAULT_SELECTION);
         list.add("XS");
         list.add("S");
         list.add("M");
@@ -28,10 +39,55 @@ public class SelectSizeActivity extends AppCompatActivity {
         list.add("XL");
         list.add("XXL");
 
+        // Create a custom adapter without the default selection as an option.
         adapter = new ArrayAdapter<String>(getApplicationContext(),
-                android.R.layout.simple_spinner_item, list);
+                android.R.layout.simple_spinner_item, list){
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent){
+                View v = null;
+                // Hide the default selection from the drop down menu.
+                if (position == 0) {
+                    TextView tv = new TextView(getContext());
+                    tv.setHeight(0);
+                    tv.setVisibility(View.GONE);
+                    v = tv;
+                }
+                else {
+                    v = super.getDropDownView(position, null, parent);
+                }
+
+                parent.setVerticalScrollBarEnabled(false);
+                return v;
+            }
+        };
+
         adapter.setDropDownViewResource(R.layout.item_spinner);
         spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selected = spinner.getSelectedItem().toString();
+
+                // If the user actually selected a valid option.
+                if (selected != DEFAULT_SELECTION){
+
+                    // Start a timer and when it reaches a set time, go to next activity.
+                    Timer timer = new Timer();
+                    timer.schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                        }
+                    }, 500);
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
     }
 }
